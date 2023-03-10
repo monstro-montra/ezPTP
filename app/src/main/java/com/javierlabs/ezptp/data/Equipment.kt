d40365894e562a46a095d739686932f1c2bdd268
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-03-10 04:16:40 -0500

    updated the firebase database and implemented spinner code for the StartFragment.kt

--- app/src/main/java/com/javierlabs/ezptp/data/Equipment.kt
+++ app/src/main/java/com/javierlabs/ezptp/data/Equipment.kt
@@ -1,25 +1,11 @@
 package com.javierlabs.ezptp.data
-import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
+data class Equipment(
+    var equipmentID: Int? = 0,
+    var type: String? = "",
+    var panel: String? = ""
+)
 
 
-data class EquipmentModel(
-    @SerializedName("equipment")
-    var equipment:List<Equipment>
-){
-    data class Equipment(
-        @SerializedName("type")
-        var type: String? = null,
-
-        @SerializedName("equipmentID")
-        var equipment_id: Int? = null,
-
-        @SerializedName("panel")
-        val panel_id: String? = null,
-    )
-
-}
 
 
 
--- app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
+++ app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
@@ -1,20 +1,18 @@
 package com.javierlabs.ezptp.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.javierlabs.ezptp.R
-import com.javierlabs.ezptp.data.JSONUtils
+import com.javierlabs.ezptp.data.Equipment
 import com.javierlabs.ezptp.databinding.FragmentStartBinding
 
-private val equipmentCollectionReference = Firebase.firestore.collection("equipment")
 class StartFragment : Fragment() {
     private var fragmentStartBinding: FragmentStartBinding? = null
     override fun onCreateView(
@@ -31,22 +29,37 @@ class StartFragment : Fragment() {
         fragmentStartBinding = binding
         val spinner = binding.equipmentsSpinner
 
-        val equipment = activity?.let { JSONUtils.getJsonDataFromAsset(it) }
+        val db = FirebaseFirestore.getInstance()
 
-        equipment?.forEach {
-            Log.e("Data Equipment", it.toString())
+        var equipment: MutableList<Equipment> = mutableListOf()
+        equipment.add(element = Equipment(80100, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80105, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80110, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80115, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80120, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80125, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80130, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80135, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80140, "Conveyor", "CP80"))
+        equipment.add(element = Equipment(80145, "Conveyor", "CP80"))
+
+        for(element: Equipment in equipment){
+            db.collection("equipment").document(element.equipmentID.toString()).set(element)
         }
 
-//        val adapter = activity?.let {
-//            ArrayAdapter<Int>(
-//                it,
-//                android.R.layout.simple_spinner_item,
-//                equipmentIDs
-//            )
-//        }
-//
-//        spinner.adapter = adapter
+        val equipmentIDs: MutableList<Int?> = mutableListOf()
+        for (element: Equipment in equipment){
+            equipmentIDs.add(element.equipmentID)
+        }
+        val adapter = activity?.let {
+            ArrayAdapter<Int>(
+                it,
+                android.R.layout.simple_spinner_dropdown_item,
+                equipmentIDs
+            )
+        }
 
+        spinner.adapter = adapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
@@ -57,12 +70,8 @@ class StartFragment : Fragment() {
                 "You selected ${adapterView.getItemAtPosition(position)}",
                 Toast.LENGTH_LONG).show()
             }
+
         }
-
-
-
-
-
 
     }
 
