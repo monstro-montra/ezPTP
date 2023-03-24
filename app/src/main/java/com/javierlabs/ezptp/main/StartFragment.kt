3d2fe7a181e037b642a6cb04e162f8025ccce6f6
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-03-24 04:28:54 -0400

    had to move some logic over to the addOnSuccessListener. the spinner actually populates from the firestore database now

--- app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
+++ app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
@@ -14,6 +14,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.javierlabs.ezptp.R
 import com.javierlabs.ezptp.data.Equipment
 import com.javierlabs.ezptp.databinding.FragmentStartBinding
+import kotlinx.coroutines.handleCoroutineException
 
 class StartFragment : Fragment() {
     private var fragmentStartBinding: FragmentStartBinding? = null
@@ -33,37 +34,50 @@ class StartFragment : Fragment() {
         val db = FirebaseFirestore.getInstance()
         val equipmentList: MutableList<Equipment> = mutableListOf()
 
-        db.collection("equipment").addSnapshotListener{ snapshot, e ->
-            //error logic
-            if (e != null) {
-                Log.w(TAG, ("Listen Failed"))
-                return@addSnapshotListener
-            }
-            if (snapshot != null) {
-                val documents = snapshot.documents
-                documents.forEach{
-                    val equipment = it.toObject(Equipment::class.java)
-                    if  (equipment != null) {
-                        equipment.equipmentID = it.id
-                        equipmentList.add(equipment)
+        db.collection("equipment")
+            .get()
+            .addOnSuccessListener { documents ->
+                var equipment: Equipment
+                for (document in documents){
+                    equipment = document.toObject(Equipment::class.java)
+                    equipmentList.add(equipment)
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+
+                val equipmentIDs: MutableList<String?> = mutableListOf()
+                equipmentList.forEach{ equipment: Equipment ->
+                    equipmentIDs.add(equipment.equipmentID)
+                }
+
+                val adapter = activity?.let {
+                    ArrayAdapter<String>(
+                        it,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        equipmentIDs
+                    )
+                }
+
+                spinner.adapter = adapter
+
+                spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
+                        Toast.makeText(activity,
+                            "You selected ${adapterView.getItemAtPosition(position)}",
+                            Toast.LENGTH_LONG).show()
                     }
                 }
             }
-        }
-        val equipmentIDs: MutableList<String?> = mutableListOf()
-        equipmentList.forEach{ equipment: Equipment ->
-            equipmentIDs.add(equipment.equipmentID)
-        }
+            .addOnFailureListener{exception ->
+                Log.w(TAG, "Error getting Documents: ", exception)
+            }
 
-        val adapter = activity?.let {
-            ArrayAdapter<String>(
-                it,
-                android.R.layout.simple_spinner_dropdown_item,
-                equipmentIDs
-            )
-        }
 
-        spinner.adapter = adapter
+
+
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
@@ -74,11 +88,6 @@ class StartFragment : Fragment() {
                 "You selected ${adapterView.getItemAtPosition(position)}",
                 Toast.LENGTH_LONG).show()
             }
-
         }
-
     }
-
-
-
 }
