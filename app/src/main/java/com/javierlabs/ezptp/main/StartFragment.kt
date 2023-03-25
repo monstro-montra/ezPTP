957d1effa627fba80e5807ba1b59b9dcae6c5040
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-03-25 04:57:19 -0400

    separated some of the logic in onViewCreated for StartFragment.kt, into different functions

--- app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
+++ app/src/main/java/com/javierlabs/ezptp/main/StartFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FirebaseFirestore
@@ -31,62 +32,62 @@ class StartFragment : Fragment() {
         val binding = FragmentStartBinding.bind(view)
         fragmentStartBinding = binding
         val spinner = binding.equipmentsSpinner
+
+        fetchEquipmentData { equipmentList ->
+            setupSpinner(spinner, equipmentList)
+        }
+    }
+
+    // fetch from Firestore database and return a mutable list of Equipment objects upon successful fetch. otherwise throw an error log
+    private fun fetchEquipmentData(onSuccess: (List<Equipment>) -> Unit) {
         val db = FirebaseFirestore.getInstance()
         val equipmentList: MutableList<Equipment> = mutableListOf()
-
         db.collection("equipment")
             .get()
             .addOnSuccessListener { documents ->
-                var equipment: Equipment
-                for (document in documents){
-                    equipment = document.toObject(Equipment::class.java)
+                for (document in documents) {
+                    val equipment = document.toObject(Equipment::class.java)
                     equipmentList.add(equipment)
                     Log.d(TAG, "${document.id} => ${document.data}")
                 }
-
-                val equipmentIDs: MutableList<String?> = mutableListOf()
-                equipmentList.forEach{ equipment: Equipment ->
-                    equipmentIDs.add(equipment.equipmentID)
-                }
-
-                val adapter = activity?.let {
-                    ArrayAdapter<String>(
-                        it,
-                        android.R.layout.simple_spinner_dropdown_item,
-                        equipmentIDs
-                    )
-                }
-
-                spinner.adapter = adapter
-
-                spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                    override fun onNothingSelected(p0: AdapterView<*>?) {
-                        TODO("Not yet implemented")
-                    }
-
-                    override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
-                        Toast.makeText(activity,
-                            "You selected ${adapterView.getItemAtPosition(position)}",
-                            Toast.LENGTH_LONG).show()
-                    }
-                }
+                onSuccess(equipmentList)
             }
-            .addOnFailureListener{exception ->
-                Log.w(TAG, "Error getting Documents: ", exception)
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
             }
+    }
 
+    //create the adapter for the spinner that will be populated from the list of equipmentIDs
+    private fun createAdapter(equipmentList: List<Equipment>): ArrayAdapter <String>? {
+        val equipmentIDs: MutableList<String?> = mutableListOf()
+        equipmentList.forEach{ equipment: Equipment ->
+            equipmentIDs.add(equipment.equipmentID) // add the equipmentID at index i from the equipmentList to the equipmentIDs list.
+        }
 
+        return activity?.let {
+            ArrayAdapter<String>(
+                it,
+                android.R.layout.simple_spinner_dropdown_item,
+                equipmentIDs
+            )
+        }
+    }
 
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+    //setup the spinner using the adapter. the adapter populates spinner, this function will set the functionality for each item.
+    private fun setupSpinner(spinner: Spinner, equipmentList: List<Equipment>){
+        val adapter = createAdapter(equipmentList)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
 
-            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long){
-                Toast.makeText(activity,
-                "You selected ${adapterView.getItemAtPosition(position)}",
-                Toast.LENGTH_LONG).show()
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (adapterView != null) {
+                    Toast.makeText(activity,
+                        "You selected ${adapterView.getItemAtPosition(position)}",
+                        Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
